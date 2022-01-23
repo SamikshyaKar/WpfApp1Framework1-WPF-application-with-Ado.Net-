9485fb02a3751a25637d7cc11e23570d9e0dcfd2
samikshya.sibani <samikshya.sibani@gmail.com>
2022-01-24 01:02:26 +0530

    Add project files.

---
+++ WpfApp1Framework1/MainWindow.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+
+
+namespace WpfApp1Framework1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //String CS = "server=(local); Database=Products; Trusted_Connection=true";
+        SqlConnection con = new SqlConnection("server=(local); Database=Products; Trusted_Connection=true");
+        SqlCommand cmd;
+        //SqlDataReader rdr;
+        string sql;
+        private void TxtProdName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void TextBox_TextChanged_1(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+            string ProdName = TxtProdName.Text;
+            string ProdPrice = TxtPrice.Text;
+            string ProdDiscount = TxtDiscount.Text;
+
+            sql = "Insert into ProductTable(PName,PDiscount,PPrice) values (@ProdName,@ProdPrice,@ProdDiscount)";
+            con.Open();
+            cmd = new SqlCommand(sql,con);
+            cmd.Parameters.AddWithValue("@ProdName", ProdName);
+            cmd.Parameters.AddWithValue("@ProdPrice", ProdPrice);
+            cmd.Parameters.AddWithValue("@ProdDiscount", ProdDiscount);
+
+            cmd.ExecuteNonQuery();
+            MessageBox.Show("Record Added");
+            con.Close();
+
+            TxtProdName.Clear();
+            TxtPrice.Clear();
+            TxtDiscount.Clear();
+            TxtProdName.Focus();
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
